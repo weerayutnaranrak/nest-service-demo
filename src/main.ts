5911393a205aa0641d2a0348f54ea53b924cd5ee
Stamp <weerayut.naranrak@gmail.com>
2022-10-21 15:16:18 +0700

    add swagger to service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API Smart Assistance')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        description: 'Default JWT Authorization',
+        type: 'http',
+        in: 'header',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'okta',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/modules/lead/lead.controller.ts
+++ src/modules/lead/lead.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { ExampleGuard } from 'src/guards/example.guard';
 import { LeadService } from './lead.service';
 
@@ -8,10 +9,12 @@ export class LeadController {
   constructor(private readonly leadService: LeadService) {}
 
   @Get()
+  @ApiBearerAuth('okta')
   @UseGuards(AuthGuard('bearer'), ExampleGuard)
+  // @UseGuards(ExampleGuard)
   findAll(@Request() req) {
     console.log(req.user);
-    return 'test';
-    // return this.leadService.findAll();
+    // return 'test';
+    return this.leadService.findAll();
   }
 }
