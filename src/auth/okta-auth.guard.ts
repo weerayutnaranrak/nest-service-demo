44fb4cc8b3a917fbabd5a17005e98fcad4366417
Stamp <weerayut.naranrak@gmail.com>
2022-10-25 17:27:44 +0700

    delete comment

--- src/auth/okta-auth.guard.ts
+++ src/auth/okta-auth.guard.ts
@@ -1,24 +1,14 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import OktaJwtVerifier from '@okta/jwt-verifier';
+
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class OktaAuthGuard implements CanActivate {
-  //   private oktaVerifier: OktaJwtVerifier;
-  //   private audience: string;
-
   constructor(
     readonly config: ConfigService,
     private readonly authService: AuthService,
-  ) {
-    // this.oktaVerifier = new OktaJwtVerifier({
-    //   issuer: config.get('OKTA_ISSUER'),
-    //   clientId: config.get('OKTA_CLIENTID'),
-    // });
-    // this.audience = config.get('OKTA_AUDIENCE');
-  }
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.getToken(request);
